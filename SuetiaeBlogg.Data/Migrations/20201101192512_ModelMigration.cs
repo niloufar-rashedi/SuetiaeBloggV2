24605f715d9ef3bdc49ef33bf17a22cd5eb1d33b
aremolif <amremolif@gmail.com>
2020-11-01 20:43:30 +0100

    fixed package-lock.json eof

---
+++ SuetiaeBlogg.Data/Migrations/20201101192512_ModelMigration.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuetiaeBlogg.Data.Migrations
+{
+    public partial class ModelMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Author_AuthorId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Author_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Author",
+                table: "Author");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Author");
+
+            migrationBuilder.RenameTable(
+                name: "Author",
+                newName: "Authors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Authors",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Authors_AuthorId",
+                table: "Comments",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Authors_AuthorId",
+                table: "Posts",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Authors_AuthorId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Authors_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Authors");
+
+            migrationBuilder.RenameTable(
+                name: "Authors",
+                newName: "Author");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Author",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Author",
+                table: "Author",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Author_AuthorId",
+                table: "Comments",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Author_AuthorId",
+                table: "Posts",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
