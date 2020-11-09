cd1958343848d3a080471c9d94fcd40a3ee8b330
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-11-09 16:21:20 +0100

    Authorization added to Swagger

--- SuetiaeBlogg/Startup.cs
+++ SuetiaeBlogg/Startup.cs
@@ -73,6 +73,31 @@ namespace SuetiaeBlogg
                     Title = "SuetiaeBlogg APIs",
                     Description = "REST APIs "
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, xmlFile);
