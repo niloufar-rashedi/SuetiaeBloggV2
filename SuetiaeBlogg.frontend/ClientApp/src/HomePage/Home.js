d99e5b338695384382fec2dd9f028fb0de2f2cf4
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-11-14 21:22:58 +0100

    Home page grid edited

--- SuetiaeBlogg.frontend/ClientApp/src/HomePage/Home.js
+++ SuetiaeBlogg.frontend/ClientApp/src/HomePage/Home.js
@@ -1,23 +1,64 @@
 import React, { Component} from 'react';
 import { Posts } from '../components/post/posts';
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
 
-
-import { Form} from 'reactstrap';
+import { Button } from 'reactstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-        <div>
-            <div>
+        <div class="Container">
                 <div class="row">
-                    <div class="col">
-                        <img src="https://images.unsplash.com/photo-1551730708-87acf0563a4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                            class="img-fluid" alt="Responsive image"
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src="https://images.unsplash.com/photo-1527156737205-c93d3dc84e87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                            alt="First slide"
                         />
-                    </div>
-                    
+                        <Carousel.Caption>
+                            <h3>How do you experience Suetiae?</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src="https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Getout of your comfort zone</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://images.unsplash.com/photo-1551730708-87acf0563a4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Housing can be a problem sometimes</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://images.unsplash.com/photo-1580677881307-5852a81368ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Celebrate Kanelbulle day with us!</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                </Carousel>                    
                 </div>
 
                 
@@ -62,63 +103,43 @@ export class Home extends Component {
 
 
 
-                <div class="container">
-                    <h3>Celebrate "kanelbullens dag" with a fantastic recipe </h3>
-                    <img src="https://images.unsplash.com/photo-1580677881307-5852a81368ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                        class="img-fluid" alt="Responsive image"
-                        href="https://www.ica.se/buffe/artikel/recept-till-kanelbullens-dag/"
-                    />
-                </div>
-                <div class="row mt-3">
-                    <div class="col">
-                        <img src="https://images.unsplash.com/photo-1526644253653-a411eaafdfe6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80"
-                            className="img-fluid rounded"
-                            alt="..."
-                        />
-                    </div>
-                    <div class="col">
-                        <p class="text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                        magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-                            </p>
-                    </div>
-                    <div class="col">
-                        <p class="text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            <div class="row">
+                <Card className="text-dark d-flex justify-content-center">
+                    <Card.Img src="https://images.unsplash.com/photo-1483288578299-bdb6421dee2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1310&q=80" alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Celebrate "kanelbullens dag" with a fantastic recipe</Card.Title>
+                        <Card.Text>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.
+                    </Card.Text>
+                        <Card.Text><a href="#" />click to learn more</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+
+            </div>
+            <div class="row d-flex justify-content-center">
+                <h1>Our Background</h1>
+                <p class="text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
                             magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,<a href="#" class="text-info"> read more </a>
-                        </p>
-                    </div>
-                </div>
 
-
-                <Form>
-                    <h3 class="text-center">subscribe to our newsletter</h3>
-                    <div class="form-row mx-auto">
-                        <div class="col-md-6 mb-3">
-                            <label for="validationServer01"></label>
-                            <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First name" required />
-                            <div class="valid-feedback">
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 mb-3">
-                            <label for="validationServerUsername"></label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="inputGroupPrepend3">@</span>
-                                </div>
-                                <input type="text" class="form-control is-invalid" id="validationServerUsername" placeholder="E-mail" aria-describedby="inputGroupPrepend3" required />
-                                <div class="invalid-feedback">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                        <div class="col-auto my-1">
-
-                        </div>
-                    </div>
-                </Form>
+                </p>
             </div>
 
-      </div>
+
+            <div class="row d-flex justify-content-center">
+                <Card className="text-center">
+                    <Card.Header>subscription</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Join our mailing list to read the latest on Suetiae</Card.Title>
+                        <Card.Text>
+
+                        </Card.Text>
+                        <Button variant="warning">Subscribe</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted"></Card.Footer>
+                </Card>
+            </div>
+            </div>
     );
   }
 }
